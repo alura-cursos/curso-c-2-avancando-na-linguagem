66f4b6520d92b92159f5ea90064553478069b703
Lucas Felix <lfelixsampaio@gmail.com>
2016-02-13 18:40:15 -0300

    Arquivos do curso

---
+++ capitulo5/forca.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
+
+void abertura() {
+    printf("***********************\n");
+    printf("*    Jogo de Forca    *\n");
+    printf("***********************\n\n");
+}
+
+void chuta() {
+    char chute;
+    printf("Qual a letra? ");
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+    tentativas++;
+}
+
+int jachutou(char letra) {
+    int achou = 0;
+
+    for(int j = 0; j < tentativas; j++) {
+        if(chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca() {
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+        int achou = jachutou(palavrasecreta[i]);
+
+        if(achou) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
+
+void escolhepalavra() {
+    sprintf(palavrasecreta, "MELANCIA");
+}
+
+int main() {
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    escolhepalavra(palavrasecreta);
+    abertura();
+
+    do {
+
+        desenhaforca(palavrasecreta, chutes, tentativas);
+
+        chuta(chutes, &tentativas);
+
+    } while(!acertou && !enforcou);
+}
